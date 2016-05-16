3fbaebd0874de04b0187e9a6faeebf915f636e26
Yuri Schimke <yuri@schimke.ee>
2016-05-16 16:30:12 -0700

    reactivesocket-cli initial checkin (#1)

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# reactivesocket-cli
-Command-line client for ReactiveSocket
+# ReactiveSocket CLI
+
+## Description
+
+Simple ReactiveSocket CLI currently for two main purposes
+
+1. Early testing of new protocol server implementations e.g. websocket
+2. Sending some basic traffic to servers built using ReactiveSocket e.g. help debug a mobile <=> server integration issue. 
+
+## Running
+
+```
+$ gradle installDist
+$ ./build/install/reactivesocket-cli/bin/reactivesocket-cli tcp://localhost:8765
+```
+
+## Caveats
+
+- This may be rewritten in C++ at some point soon when transports are ready in C++.
+- This isn't a general purpose tool like curl for making web requests, but rather a manul test tool crutch.
+- It will probably never have all the features you want for testing application functionality of ReactiveSocket applications. 
+
---
+++ src/main/java/io/reactivesocket/cli/PayloadImpl.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reactivesocket.cli;
+
+import io.reactivesocket.Payload;
+import io.reactivesocket.internal.frame.ByteBufferUtil;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class PayloadImpl implements Payload // some JDK shoutout
+{
+  private ByteBuffer data;
+  private ByteBuffer metadata;
+
+  public PayloadImpl(final String data, final String metadata) {
+    if (null == data) {
+      this.data = ByteBuffer.allocate(0);
+    } else {
+      this.data = byteBufferFromUtf8String(data);
+    }
+
+    if (null == metadata) {
+      this.metadata = ByteBuffer.allocate(0);
+    } else {
+      this.metadata = byteBufferFromUtf8String(metadata);
+    }
+  }
+
+  public static ByteBuffer byteBufferFromUtf8String(final String data)
+  {
+    final byte[] bytes = data.getBytes(StandardCharsets.UTF_8);
+    return ByteBuffer.wrap(bytes);
+  }
+
+  public boolean equals(Object obj)
+  {
+    System.out.println("equals: " + obj);
+    final Payload rhs = (Payload) obj;
+
+    return (data.equals(rhs.getData())) &&
+        (metadata.equals(rhs.getMetadata()));
+  }
+
+  public ByteBuffer getData() {
+    return data;
+  }
+
+  public ByteBuffer getMetadata() {
+    return metadata;
+  }
+}
