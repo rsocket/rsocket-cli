357a093dab87526b9180f5266a12bc3577b7dca5
Yuri Schimke <yuri@schimke.ee>
2017-05-31 11:18:05 +0100

    wss support

--- src/main/java/io/rsocket/cli/ConnectionHelper.java
+++ src/main/java/io/rsocket/cli/ConnectionHelper.java
@@ -19,6 +19,7 @@ import io.rsocket.transport.netty.client.TcpClientTransport;
 import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import io.rsocket.transport.netty.server.TcpServerTransport;
 import io.rsocket.transport.netty.server.WebsocketServerTransport;
+import reactor.ipc.netty.http.client.HttpClient;
 import reactor.ipc.netty.tcp.TcpClient;
 import reactor.ipc.netty.tcp.TcpServer;
 
@@ -29,7 +30,11 @@ public class ConnectionHelper {
         if ("tcp".equals(uri.getScheme())) {
             return TcpClientTransport.create(TcpClient.create(uri.getHost(), uri.getPort()));
         } else if ("ws".equals(uri.getScheme())) {
-            return WebsocketClientTransport.create(uri.getHost(), uri.getPort());
+            return WebsocketClientTransport.create(uri.getHost(), wsPort(uri));
+        } else if ("wss".equals(uri.getScheme())) {
+            HttpClient httpClient =
+                    HttpClient.create(options -> options.sslSupport().connect(uri.getHost(), wssPort(uri)));
+            return WebsocketClientTransport.create(httpClient);
         } else {
             throw new UnsupportedOperationException("uri unsupported: " + uri);
         }
@@ -39,9 +44,19 @@ public class ConnectionHelper {
         if ("tcp".equals(uri.getScheme())) {
             return TcpServerTransport.create(TcpServer.create(uri.getHost(), uri.getPort()));
         } else if ("ws".equals(uri.getScheme())) {
-            return WebsocketServerTransport.create(uri.getHost(), uri.getPort());
+            return WebsocketServerTransport.create(uri.getHost(), wsPort(uri));
+        } else if ("wss".equals(uri.getScheme())) {
+            throw new UnsupportedOperationException("server not supported for WSS");
         } else {
             throw new UnsupportedOperationException("uri unsupported: " + uri);
         }
+    }
+
+    private static int wsPort(URI uri) {
+        return uri.getPort() == -1 ? 80 : uri.getPort();
+    }
+
+    private static int wssPort(URI uri) {
+        return uri.getPort() == -1 ? 443 : uri.getPort();
     }
 }
