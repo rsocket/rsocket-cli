8284357454fa0a4574663b9ab00be98236340bf8
Yuri Schimke <yschimke@fb.com>
2017-07-08 12:33:32 +0100

    cleanup

--- src/test/java/io/rsocket/util/HeaderUtilTest.java
+++ src/test/java/io/rsocket/util/HeaderUtilTest.java
@@ -3,6 +3,9 @@ package io.rsocket.util;
 import static org.junit.Assert.assertEquals;
 
 import io.rsocket.cli.util.HeaderUtil;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Map;
 import org.junit.Test;
@@ -14,14 +17,14 @@ public class HeaderUtilTest {
   }
 
   @Test
-  public void testFileValue() {
-    assertEquals("value", HeaderUtil.stringValue("@src/test/resources/value.txt"));
+  public void testFileValue() throws URISyntaxException {
+    assertEquals("value", HeaderUtil.stringValue("@" + path("value.txt")));
   }
 
   @Test
-  public void headerMap() {
+  public void headerMap() throws URISyntaxException {
     Map<String, String> map =
-        HeaderUtil.headerMap(Arrays.asList("A: a", "B:b", "C: @src/test/resources/value.txt"));
+        HeaderUtil.headerMap(Arrays.asList("A: a", "B:b", "C: @" + path("value.txt")));
 
     assertEquals(3, map.size());
     assertEquals("a", map.get("A"));
@@ -30,12 +33,16 @@ public class HeaderUtilTest {
   }
 
   @Test
-  public void headerFileMap() {
-    Map<String, String> map =
-        HeaderUtil.headerMap(Arrays.asList("@src/test/resources/headers.txt"));
+  public void headerFileMap() throws URISyntaxException {
+    Map<String, String> map = HeaderUtil.headerMap(Arrays.asList("@" + path("headers.txt")));
 
     assertEquals(2, map.size());
     assertEquals("a", map.get("A"));
     assertEquals("b", map.get("B"));
+  }
+
+  private String path(String resourceName) throws URISyntaxException {
+    URL resource = getClass().getClassLoader().getResource(resourceName);
+    return Paths.get(resource.toURI()).toFile().getPath();
   }
 }
