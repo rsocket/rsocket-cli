f3036153a242681bc14140217842f86969d76b17
Yuri Schimke <yuri@schimke.ee>
2017-07-20 09:31:53 -0700

    fix sending (#51)

--- src/main/java/io/rsocket/cli/SDF.java
+++
@@ -1,7 +0,0 @@
-package io.rsocket.cli;
-
-public class SDF {
-  public static void main(String[] args) throws Exception {
-    Main.main("--debug", "--rr", "tcp://localhost:9898", "-i", "Client");
-  }
-}
