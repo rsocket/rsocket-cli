fa165cae18a24fae8dcb523e4b639a9f42da6e98
Yuri Schimke <yuri@schimke.ee>
2018-07-16 17:27:36 +0100

    dep upgrade

--- src/main/kotlin/io/rsocket/cli/http2/Http2UriHandler.kt
+++ src/main/kotlin/io/rsocket/cli/http2/Http2UriHandler.kt
@@ -1,16 +1,21 @@
 package io.rsocket.cli.http2
 
+import io.rsocket.Closeable
 import io.rsocket.transport.ClientTransport
+import io.rsocket.transport.ServerTransport
 import io.rsocket.uri.UriHandler
 import org.eclipse.jetty.http.HttpScheme
 import java.net.URI
 import java.util.Optional
+import java.util.Optional.empty
 import java.util.Optional.of
 
 class Http2UriHandler : UriHandler {
   override fun buildClient(uri: URI?): Optional<ClientTransport> =
     when {
       HttpScheme.HTTPS.`is`(uri!!.scheme) || HttpScheme.HTTP.`is`(uri.scheme) -> of(Http2ClientTransport(uri))
-      else -> super.buildClient(uri)
+      else -> Optional.empty()
     }
+
+  override fun buildServer(uri: URI?): Optional<ServerTransport<Closeable>> = empty()
 }
