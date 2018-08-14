1e6bfd49b7130e0abf4ce08056018224f92a0fdb
Yuri Schimke <yuri@schimke.ee>
2018-08-14 14:37:14 +0100

    release tool

--- create_release.sh
+++ create_release.sh
@@ -30,7 +30,7 @@ RELEASE_BODY=$(cat <<EOF
 EOF
 )
 
-RELEASE_ID=$(oksocial -d "$RELEASE_BODY" https://api.github.com/repos/rsocket/rsocket-cli/releases | jq .id)
+RELEASE_ID=$(okurl -d "$RELEASE_BODY" https://api.github.com/repos/rsocket/rsocket-cli/releases | jq .id)
 
 echo Created "https://api.github.com/repos/rsocket/rsocket-cli/releases/${RELEASE_ID}"
 
